6bd195315de65759db627c844d907f88c3c1df88
hooh00 <98064793+hooh00@users.noreply.github.com>
2022-01-25 14:03:27 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
 
+input_str = str(input('Enter a word:\n'))
+
+total_score = 0
+for letter in input_str.lower():
+  i = letters.index(letter)
+  score = points[i]
+  total_score = total_score + score
+print('Total score: ' + str(total_score))
