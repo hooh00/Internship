442a2c774179341fa2e092dbda89a4cca50ffa10
hooh00 <98064793+hooh00@users.noreply.github.com>
2022-02-23 09:40:31 -0800

    Create array.py

---
+++ array.py
@@ -0,0 +1,18 @@
+from numpy import *
+arr1 = array([
+  [1,2,3,6,2,9],
+  [4,5,6,7,5,3]
+  
+])
+
+arr2 = arr1.flatten()
+#from 2 dimension to 1 dimension
+
+arr3 = arr2.reshape(3,4)
+#make it into a 3 dimensional array
+
+print(arr1)
+#print(arr1.dtype) know what type of data you're working with
+#print(arr1.ndim) number of dimensions
+#print(arr1.shape) number of rows and columns 
+#print(arr1.size) size of the entire block
