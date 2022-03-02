6213c954149133d13465d07acc0630c28d6cba2f
hooh00 <98064793+hooh00@users.noreply.github.com>
2022-03-02 14:52:34 -0800

    Create matrix2.py

---
+++ matrix2.py
@@ -0,0 +1,12 @@
+from numpy import *
+
+m1 = matrix('1 2 3 ; 4 5 6 ; 7 8 9')
+m2 = matrix('1 2 3 ; 2 5 6 ; 5 8 9')
+
+m3 = m1 + m2;
+#add two matrices
+
+m4 = m1 * m2;
+#multiply two matrices
+
+print(m4)
