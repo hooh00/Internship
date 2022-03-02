c0477920205bfe667ff1c6f5799ca3f7e57dda14
hooh00 <98064793+hooh00@users.noreply.github.com>
2022-03-02 14:06:47 -0800

    Create matrix1.py

---
+++ matrix1.py
@@ -0,0 +1,18 @@
+from numpy import *
+
+#arr1 = array([
+  #[1,2,3,],
+  #[4,5,6,],
+  #[7,8,9]
+  
+#])
+
+#m = matrix(arr1)
+
+m = matrix('1 2 3 ; 4 5 6 ; 7 8 9')
+#same thing as above (you can separate each one with semicolon)
+
+print(m)
+#print(diagonal(m)) only prints the diagonals (in this case 1,5,9)
+#print(m.min()) minimum
+#print(m.max()) maximum
