12c1c252dc6cff45a596983d84772c050d2ee66a
hooh00 <98064793+hooh00@users.noreply.github.com>
2022-04-20 12:58:08 -0700

    Create plot2.py

---
+++ plot2.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Define the sinc function, with output for x=0 defined
+# as a special case to avoid division by zero. The code
+# below defining the sinc function is developed and
+# explained in Chapter 7, Section 1.
+def s(x):
+  a = np.where(x==0., 1., np.sin(x)/x)
+  return a
+
+# create arrays for plotting
+x = np.arange(0., 10., 0.1)
+y = np.exp(x)
+
+t = np.linspace(-10., 10., 100)
+z = s(t)
+
+# create a figure window
+fig = plt.figure(1, figsize=(9,8))
+
+# subplot: linear plot of exponential
+ax1 = fig.add_subplot(2,2,1)
+ax1.plot(x, y)
+ax1.set_xlabel('time (ms)')
+ax1.set_ylabel('distance (mm)')
+ax1.set_title('exponential')
+
+# subplot: semi-log plot of exponential
+ax2 = fig.add_subplot(2,2,2)
+ax2.plot(x, y)
+ax2.set_yscale('log')
+ax2.set_xlabel('time (ms)')
+ax2.set_ylabel('distance (mm)')
+ax2.set_title('exponential')
+
+# subplot: wide subplot of sinc function
+ax3 = fig.add_subplot(2,1,2)
+ax3.plot(t, z, 'r')
+ax3.axhline(color='gray')
+ax3.axvline(color='gray')
+ax3.set_xlabel('angle (deg)')
+ax3.set_ylabel('electric field')
+ax3.set_title('sinc function')
+
+# Adjusts white space to avoid collisions between subplots
+fig.tight_layout()
+plt.show()
